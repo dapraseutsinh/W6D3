6b1dc6894d0a88a1f7829757e94676113970c4f4
Darren <dapraseutsinh@gmail.com>
2018-08-29 17:31:33 -0400

    Update tweet contoller

--- skeleton/frontend/follow_toggle.js
+++ skeleton/frontend/follow_toggle.js
@@ -1,7 +1,7 @@
 function FollowToggle(el, args) {
   this.$el = $(el);
-  this.userId = this.$el.data('user-id');
-  this.followState = this.$el.data('initial-follow-state');
+  this.userId = this.$el.data('user-id'); || options.userId;
+  this.followState = this.$el.data('initial-follow-state'); || options.followState);
   this.render();
   this.$el.on('click', this.handleClick.bind(this));
 }
