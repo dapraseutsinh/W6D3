dbfed3213aa48cfad09a654e172f127b3b18ce77
Darren <dapraseutsinh@gmail.com>
2018-08-29 17:45:27 -0400

    Add tweet_compose class

---
+++ skeleton/frontend/tweet_compose.js
@@ -0,0 +1,17 @@
+class TweetCompose {
+  constructor(el) {
+    this.$el = $(el);
+    this.$el.on('submit', this.submit.bind(this));
+
+
+  }
+
+  function submit(event) {
+    event.preventDefault();
+    const data = this.$el.serializeJSON();
+    APIUtil.createTweet(data).then(tweet => this.handleSuccess(tweet));
+  }
+
+}
+
+module.exports = TweetCompose;
