b9d4d4e82c61d46bf1726201b508af6694a1c3d3
Darren <dapraseutsinh@gmail.com>
2018-08-29 17:14:46 -0400

    Update users_search

--- skeleton/frontend/users_search.js
+++ skeleton/frontend/users_search.js
@@ -16,8 +16,45 @@ handleInput(event) {
      .then(users => this.renderResults(users));
  }
 
- function renderResults() {
-   // empty ul.users
-   // build li w JQ w a tag to user
-   // append to ul
+ function renderResults(users) {
+   // // empty ul.users
+   // this.$ul.empty();
+   // // build li w JQ w a tag to user
+   // for (let i = 0; i < users.length; i++) {
+   //   const user = users[i];
+   //   let $a = $('<a></a>');
+   //   $a.text(`@{user.username}`);
+   //   $a.attr('href', `/users/${user.id}`);
+   //
+   //   const$followToggle = $('<button></button');
+   //   new FollowToggle($followToggle, {
+   //     userId: user.id,
+   //     followState: user.followed ? 'followed' : 'unfollowed'
+   //   });
+   // // append to ul
+   //  const $li = $('<li></li>');
+   //  $li.append($a);
+   //  $li(.append($followToggle));
+   //  this.$ul.append($li);
+
+   this.$ul.empty();
+
+for (let i = 0; i < users.length; i++) {
+  const user = users[i];
+
+  let $a = $('<a></a>');
+  $a.text(`@${user.username}`);
+  $a.attr('href', `/users/${user.id}`);
+
+  const $followToggle = $('<button></button>');
+  new FollowToggle($followToggle, {
+    userId: user.id,
+    followState: user.followed ? 'followed' : 'unfollowed'
+  });
+
+  const $li = $('<li></li>');
+  $li.append($a);
+  $li.append($followToggle);
+
+  this.$ul.append($li);
  }
